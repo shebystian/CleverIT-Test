8952d008a09f21a54d9ad94903e2b42676ff151e
Sebastián Sánchez <sebastian.sanchez@viajesfalabella.com>
2020-05-27 14:49:47 -0400

    Primer commit

---
+++ src/main/java/cl/cleverit/CleveritTestApplication.java
@@ -0,0 +1,13 @@
+package cl.cleverit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleveritTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleveritTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/cleverit/CleveritTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.cleverit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleveritTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
